6eb9e25272b023eace3db358c21f7d05a1e34e9d
oneheed <oneheed@gmail.com>
2021-04-01 18:26:49 +0800

    Add Redis pub sub Sample

---
+++ RedisPubSubSample/App.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Redis.Core;
+using StackExchange.Redis;
+
+namespace RedisPubSubSample
+{
+    class App : BaseConnectionMultiplexer
+    {
+        public App(
+            ILoggerFactory factory,
+            IHostApplicationLifetime appLifetime,
+            IConnectionMultiplexer connection) : base(factory.CreateLogger<App>(), appLifetime, connection, (int)RedisType.String)
+        {
+        }
+
+        public override async Task ExcuteFunc(CancellationToken cancellationToken)
+        {
+            var sub = this._connection.GetSubscriber();
+            var readedCount = 0;
+
+            sub.Subscribe("SequentialChannel", (channel, message) =>
+            {
+                this._logger.LogInformation($"{DateTime.Now:yyyy-MM-dd hh:mm:ss.fff} - Received message {message}");
+
+                readedCount++;
+            });
+        }
+    }
+}
---
+++ RedisPubSubSample/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Redis.Core;
+
+namespace RedisPubSubSample
+{
+    class Program
+    {
+        private static IServiceCollection AddHostedService(IServiceCollection services)
+            => services.AddHostedService<App>();
+
+        static void Main(string[] args)
+        {
+            Redis.Core.HostBuilder.CreateHostBuilder(args, CreateType.ConnectionMultiplexer, AddHostedService)
+                .RunConsoleAsync();
+        }
+    }
+}
